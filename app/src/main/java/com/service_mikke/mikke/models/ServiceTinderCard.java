40f7893d3fca33bcbea36d808bcb96b3c9158463
nandate <o238049t@mail.kyutech.jp>
2017-03-30 11:29:16 +0900

    Associate card swipe event with firebase

--- app/src/main/java/com/service_mikke/mikke/models/ServiceTinderCard.java
+++ app/src/main/java/com/service_mikke/mikke/models/ServiceTinderCard.java
@@ -4,6 +4,13 @@ import android.content.Context;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.mindorks.placeholderview.SwipePlaceHolderView;
 import com.mindorks.placeholderview.annotations.Click;
 import com.mindorks.placeholderview.annotations.Layout;
@@ -16,6 +23,10 @@ import com.mindorks.placeholderview.annotations.swipe.SwipeOut;
 import com.mindorks.placeholderview.annotations.swipe.SwipeOutState;
 import com.service_mikke.mikke.R;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by takuya on 3/19/17.
  */
@@ -23,6 +34,8 @@ import com.service_mikke.mikke.R;
 
 @Layout(R.layout.service_card_view)
 public class ServiceTinderCard {
+
+
     @View(R.id.service_name)
     private TextView service_name_view;
 
@@ -59,9 +72,12 @@ public class ServiceTinderCard {
     @SwipeOut
     private void onSwipedOut(){
         Log.d("EVENT", "onSwipedOut");
-        User user = User.getInstance();
+        addTagPoint(mService);
+
+
+        /*User user = User.getInstance();
         user.addUsedService(mService);
-        System.out.println(user.getSelected_genres());
+        System.out.println(user.getSelected_genres());*/
     }
 
     @SwipeCancelState
@@ -82,6 +98,40 @@ public class ServiceTinderCard {
     @SwipeOutState
     private void onSwipeOutState(){
         Log.d("EVENT", "onSwipeOutState");
+    }
+
+
+    private void addTagPoint(Service mService){
+
+        final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        final String mUserId =mFirebaseUser.getUid();
+        final ArrayList<String> tags = mService.getTags();
+
+        mDatabase.child("users").child(mUserId).child("tags_point").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot snapshot:dataSnapshot.getChildren()){
+                    String tag_name = snapshot.getKey();
+                    if(tags.indexOf(tag_name)>0){
+                        Integer point = snapshot.getValue(Integer.class);
+                        point = point + 1;
+                        Map<String,Object> map = new HashMap<String, Object>();
+                        map.put(tag_name,point);
+                        mDatabase.child("users").child(mUserId).child("tags_point").updateChildren(map);
+                        
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
     }
 }
