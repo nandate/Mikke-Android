5e6c9f482c0222eb8eea6464904269908bedc1e9
nandate <o238049t@mail.kyutech.jp>
2017-05-10 00:21:22 +0900

    soon fragment

--- app/src/main/java/com/service_mikke/mikke/activities/FullServicesActivity.java
+++ app/src/main/java/com/service_mikke/mikke/activities/FullServicesActivity.java
@@ -4,6 +4,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.webkit.WebView;
@@ -15,6 +17,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.service_mikke.mikke.R;
+import com.service_mikke.mikke.fragments.SoonFragment;
 
 /**
  * Created by takuya on 4/8/17.
@@ -62,13 +65,11 @@ public class FullServicesActivity extends AppCompatActivity{
         description_view = (TextView)findViewById(R.id.full_service_description);
         description_view.setText(description);
 
-        webView = (WebView)findViewById(R.id.web_view);
 
         link_button = (Button)findViewById(R.id.link_button);
         link_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                webView.loadUrl(link);
             }
         });
     }
